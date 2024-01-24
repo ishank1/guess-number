66ce8f41131bc40f478c4b86b5ff1e053fba45e5
ishank1 <153811351+ishank1@users.noreply.github.com>
2024-01-24 07:48:18 -0800

    Create game.py

---
+++ game.py
@@ -0,0 +1,19 @@
+import random
+number=random.randint(1,10)
+name=input("what is your name?")
+print("hello",name,"you get 3 chances to guess a number so be careful and guess correctly")
+numguess=0
+while numguess<3:
+  numguess+=1
+  guess=int(input("enter your guess"))
+  if guess<number:
+    print("go a little higher")
+  if guess>number:
+    print("go a little lower")
+  if guess==number:
+    break
+
+if guess==number:
+  print("you are correct")
+else:
+  print("fuck you the number is ",number)
